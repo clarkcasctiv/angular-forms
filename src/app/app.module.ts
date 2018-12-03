69c9d5f95d7c28053b36d55996e45aa8b6c7235a
Pujan Bajra <pujanisme@gmail.com>
2018-12-03 12:17:38 +0545

    Reactive Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+ import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
@@ -12,6 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
+    // ReactiveFormsModule,
     HttpModule
   ],
   providers: [],
